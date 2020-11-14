0d8dcceaf85962a80e0b793a0dbc22419294039c
Guy Barnard <guy@barnard.io>
2020-11-14 13:53:29 -0600

    intial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Github Actions for Microsoft Teams
+
+### Usage
+
+1. Add `MS_TEAMS_WEBHOOK_URI` on your repository's configs on Settings > Secrets. See [webhook URI](https://docs.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook) for further information
+
+2) Add a new `step` on your workflow code as first or last step of workflow job:
+
+```yaml
+name: Github Action for Microsoft Teams
+
+on: [push]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: actions/checkout@v2
+      - uses: opsless/ms-teams-github-actions@main
+        if: always() # to let this step always run even if previous step failed
+        with:
+          github-token: ${{ github.token }}
+          webhook-uri: ${{ secrets.MS_TEAMS_WEBHOOK_URI }}
+          position: start # or finish
+```
---
+++ src/main.ts
@@ -0,0 +1,195 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { Context } from '@actions/github/lib/context'
+import * as axios from 'axios'
+
+async function run(): Promise<void> {
+  try {
+    const token = core.getInput('github-token')
+    if (!token) {
+      core.setFailed("'github-token' input can't be empty")
+      return
+    }
+
+    const webhookUri = core.getInput('webhook-uri')
+    if (!webhookUri) {
+      core.setFailed("'webhook-uri' input can't be empty")
+      return
+    }
+
+    const position = core.getInput('position')
+    if (!position || ['start', 'finish'].indexOf(position) == -1) {
+      core.setFailed("'position' input must be 'start' or 'finish'")
+      return
+    }
+
+    const context: Context = github.context
+
+    switch (position) {
+      case 'start': 
+        return notifyStarted(token, webhookUri, context)
+      case 'finish':
+        return notifyFinished(token, webhookUri, context)
+    }
+    
+
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+
+async function notifyStarted(token: string, webhookUri: string, context: Context): Promise<void> {
+ 
+  try {
+   
+    const octokit = github.getOctokit(token)
+
+    const wr = await octokit.actions.getWorkflowRun({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      run_id: context.runId
+    })
+
+    const themeColor ='90C978'
+
+    const webhookBody = {
+      '@type': 'MessageCard',
+      '@context': 'http://schema.org/extensions',
+      themeColor: `${themeColor}`,
+      summary: `${context.payload.repository?.full_name} workflow started`,
+      sections: [
+        {
+          activityTitle: `Workflow '${context.workflow}' #${context.runNumber} started`,
+          activitySubtitle: `on [${context.payload.repository?.full_name}](${context.payload.repository?.html_url})`,
+          facts: [
+            {
+              name:
+              context.eventName === 'pull_request' ? 'Pull request' : 'Branch',
+              value:
+              context.eventName === 'pull_request'
+                  ? `[${context.payload.pull_request?.html_url}](${context.payload.pull_request?.html_url})`
+                  : `[${context.payload.repository?.html_url}/tree/${context.ref}](${context.payload.repository?.html_url}/tree/${context.ref})`
+            },
+            {
+              name: 'Workflow run details',
+              value: `[${wr.data.html_url}](${wr.data.html_url})`
+            }
+          ],
+          markdown: true
+        }
+      ]
+    }
+    const response = await axios.default.post(webhookUri, webhookBody)
+    core.debug(JSON.stringify(response.data))
+
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+async function notifyFinished(token: string, webhookUri: string, context: Context): Promise<void> {
+  try {
+    const token = core.getInput('github-token')
+    if (!token) {
+      core.setFailed("'github-token' input can't be empty")
+      return
+    }
+
+    const webhookUri = core.getInput('webhook-uri')
+    if (!webhookUri) {
+      core.setFailed("'webhook-uri' input can't be empty")
+      return
+    }
+
+    const position = core.getInput('position')
+    if (!position || ['start', 'finish'].indexOf(position) == -1) {
+      core.setFailed("'position' input must be 'start' or 'finish'")
+      return
+    }
+
+    const context = github.context
+    const octokit = github.getOctokit(token)
+
+    const jobList = await octokit.actions.listJobsForWorkflowRun({
+      repo: context.repo.repo,
+      owner: context.repo.owner,
+      run_id: context.runId
+    })
+
+    const jobs = jobList.data.jobs
+
+    const job = jobs.find(j => j.name === context.job)
+
+    const stoppedStep = job?.steps.find(
+      s =>
+        s.conclusion === 'failure' ||
+        s.conclusion === 'timed_out' ||
+        s.conclusion === 'cancelled' ||
+        s.conclusion === 'action_required'
+    )
+
+    const lastStep = stoppedStep
+      ? stoppedStep
+      : job?.steps.reverse().find(s => s.status === 'completed')
+
+    const wr = await octokit.actions.getWorkflowRun({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      run_id: context.runId
+    })
+
+    core.debug(JSON.stringify(wr.data))
+
+    const repository_url = context.payload.repository?.html_url
+    const commit_author = context.actor
+
+    const themeColor =
+      lastStep?.conclusion === 'success'
+        ? '90C978'
+        : lastStep?.conclusion === 'cancelled'
+        ? 'FFF175'
+        : 'C23B23'
+    const conclusion =
+      lastStep?.conclusion === 'success'
+        ? 'SUCCEEDED'
+        : lastStep?.conclusion === 'cancelled'
+        ? 'CANCELLED'
+        : 'FAILED'
+
+    const webhookBody = {
+      '@type': 'MessageCard',
+      '@context': 'http://schema.org/extensions',
+      themeColor: `${themeColor}`,
+      summary: `${commit_author} commited new changes`,
+      sections: [
+        {
+          activityTitle: `Workflow '${context.workflow}' #${context.runNumber} ${conclusion}`,
+          activitySubtitle: `on [${context.payload.repository?.full_name}](${repository_url})`,
+          facts: [
+            {
+              name:
+              context.eventName === 'pull_request' ? 'Pull request' : 'Branch',
+              value:
+              context.eventName === 'pull_request'
+                  ? `[${context.payload.pull_request?.html_url}](${context.payload.pull_request?.html_url})`
+                  : `[${context.payload.repository?.html_url}/tree/${context.ref}](${context.payload.repository?.html_url}/tree/${context.ref})`
+            },
+            {
+              name: 'Workflow run details',
+              value: `[${wr.data.html_url}](${wr.data.html_url})`
+            }
+          ],
+          markdown: true
+        }
+      ]
+    }
+    const response = await axios.default.post(webhookUri, webhookBody)
+    core.debug(JSON.stringify(response.data))
+
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
